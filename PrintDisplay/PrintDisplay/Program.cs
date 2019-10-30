7635314780fd5d90c273da4621ec7dd28db26fdf
Nagaraj <nisarga.nagaraj@siemens.com>
2019-10-30 14:54:06 +0530

    updated program.cs

---
+++ PrintDisplay/PrintDisplay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!, this is my demo app");
+        }
+    }
+}
